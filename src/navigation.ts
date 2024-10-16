2781c63442c1aea5e733b3d62d4dec5d3c5ba4e6
Brendan Thompson <github@blt.is>
2024-10-16 20:50:14 +1100

    ci: run prettier update
    Signed-off-by: Brendan Thompson <github@blt.is>

--- src/navigation.ts
+++ src/navigation.ts
@@ -3,23 +3,19 @@ import { getPermalink, getAsset } from './utils/permalinks';
 export const headerData = {
   links: [
     {
-      text: "Features",
-      href: getPermalink('/features')
-    }
+      text: 'Features',
+      href: getPermalink('/features'),
+    },
   ],
   // actions: [{ text: 'Download', href: 'https://github.com/onwidget/astrowind', target: '_blank' }],
 };
 
 export const footerData = {
-  links: [
-    
-  ],
+  links: [],
   secondaryLinks: [
     { text: 'Terms', href: getPermalink('/terms') },
     { text: 'Privacy Policy', href: getPermalink('/privacy') },
   ],
-  socialLinks: [
-    { ariaLabel: 'RSS', icon: 'tabler:rss', href: getAsset('/rss.xml') }
-  ],
+  socialLinks: [{ ariaLabel: 'RSS', icon: 'tabler:rss', href: getAsset('/rss.xml') }],
   footNote: '© 2024 Wraith Industries',
 };
